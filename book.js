0d74b1cf3b823d0f29cce1c1246094ef127d8e9c
ysinghshashank <ysinghshashank1998@gmail.com>
2024-03-20 23:51:19 +0530

    js Practice

---
+++ book.js
@@ -0,0 +1,13 @@
+// const book = {
+//     name: 'a tale of two cities',
+//     author: 'Charles Dickens',
+//     price: 150
+// };
+// module.exports = book;
+console.log("inside book.js");
+const bookDetail = {
+    name: "hello",
+    author: "world",
+};
+//console.log(bookDetail);
+module.exports = bookDetail;
